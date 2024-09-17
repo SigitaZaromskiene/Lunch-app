b0c3540d247d03a52fb59363a19bdcf3821d0c5e
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2024-09-17 10:50:55 +0300

    update

--- src/components/Menu/MenuList/MenuList.tsx
+++ src/components/Menu/MenuList/MenuList.tsx
@@ -1,12 +1,12 @@
-import { ReactElement } from "react";
-import mealsData from "../../../../data/db.json";
-import { selectMenuImg } from "../../../helpers/selectMenuImg";
-import MenuListDescription from "./MenuListDescribtion";
-import MenuListAddToCart from "./MenuListAddToCart";
-import spicyIcon from "../../../assets/img/spicy.svg";
-import veggetarianIcon from "../../../assets/img/vegetarian.svg";
-import styles from "./MenuList.module.scss";
-import classNames from "classnames/bind";
+import { ReactElement } from 'react';
+import mealsData from '../../../../data/db.json';
+import { selectMenuImg } from '../../../helpers/selectMenuImg';
+import MenuListDescription from './MenuListDescribtion';
+import MenuListAddToCart from './MenuListAddToCart';
+import spicyIcon from '../../../assets/img/spicy.svg';
+import veggetarianIcon from '../../../assets/img/vegetarian.svg';
+import styles from './MenuList.module.scss';
+import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
 
@@ -25,27 +25,29 @@ const MenuHeader = (): ReactElement => {
   return (
     <>
       {meals.map((meal: MealProps) => (
-    <li key={meal.id} className={cx("menu-list")}>
-    <div className={cx("menu-list-header")}>
-      <div className={cx("menu-list-header__image")}>
-        {selectMenuImg(meal.title)}
-      </div>
-      <div className={cx("menu-list-header__img-container")}></div>
-      <h1 className={cx("menu-list-header__text")}>
-        {meal.title}
-        <div className={cx("menu-list-header__text-icon")}>
-          {meal.vegetarian && (
-            <img src={veggetarianIcon} alt="Vegetarian food icon" />
-          )}
-          {meal.spicy && <img src={spicyIcon} alt="Spicy food icon" />}
-          {!meal.spicy && !meal.vegetarian && <p>&nbsp;</p>}
-        </div>
-      </h1>
-    </div>
-    <MenuListDescription description={meal.description} mealId={meal.id} />
-    <MenuListAddToCart price={meal.price} />
-  </li>
-  
+        <li key={meal.id} className={cx('menu-list')}>
+          <div className={cx('menu-list-header')}>
+            <div className={cx('menu-list-header__image')}>
+              {selectMenuImg(meal.title)}
+            </div>
+            <div className={cx('menu-list-header__img-container')}></div>
+            <h1 className={cx('menu-list-header__text')}>
+              {meal.title}
+              <div className={cx('menu-list-header__text-icon')}>
+                {meal.vegetarian && (
+                  <img src={veggetarianIcon} alt="Vegetarian food icon" />
+                )}
+                {meal.spicy && <img src={spicyIcon} alt="Spicy food icon" />}
+                {!meal.spicy && !meal.vegetarian && <p>&nbsp;</p>}
+              </div>
+            </h1>
+          </div>
+          <MenuListDescription
+            description={meal.description}
+            mealId={meal.id}
+          />
+          <MenuListAddToCart price={meal.price} />
+        </li>
       ))}
     </>
   );
